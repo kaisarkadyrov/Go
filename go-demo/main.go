c338af7636df064a71607ec9c28414f94e2bbcf4
Kaisar Kadyrov <ka_kadyrov@kbtu.kz>
2026-02-06 16:16:44 +0500

    more_practice

--- go-demo/main.go
+++ go-demo/main.go
@@ -10,13 +10,28 @@ func main() {
 
 	// var userHeight = 1.8
 	// var userWeight float64 = 100
+	// var lol int = 50
+	var userHeight float64
+	var userWeight float64
 
-	userHeight := 1.8
-	userWeight := 100.0
+	// var a, b, c, d int = 1, 3, 5, 7
 
+	fmt.Print("Enter your height: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Enter your weight: ")
+	fmt.Scan(&userWeight)
+
+	const IMT_power = 2
 	// var IMT = userWeight / math.Pow(userHeight, 2)
-	IMT := userWeight / math.Pow(userHeight, 2)
+	IMT := userWeight / math.Pow(userHeight, IMT_power)
 
-	fmt.Print(IMT)
+	fmt.Printf("Your IMT: %.0f", IMT) // выводит форматированную строку, выводит точное число
 
+	var i string = "Hello"
+	var j int = 15
+
+	fmt.Printf("i has value: %v and type: %T\n", i, i)
+	fmt.Printf("j has value: %v and type: %T", j, j)
+
+	fmt.Printf("type of the i: %T", i)
 }
