216b8896c5456de3149288032a433ecd49434e6a
Kaisar Kadyrov <ka_kadyrov@kbtu.kz>
2026-04-01 21:52:50 +0500

    Practice 01.04.2026

---
+++ go-demo/practice/brackets/valid_brackets.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func isValid(s string) bool {
+	stack := []rune{}
+
+	for _, ch := range s {
+		switch ch {
+		case '(', '[', '{':
+			stack = append(stack, ch)
+		case ')', ']', '}':
+			if len(stack) == 0 {
+				return false
+			}
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			if (ch == ')' && top != '(') ||
+				(ch == ']' && top != '[') ||
+				(ch == '}' && top != '{') {
+				return false
+			}
+		}
+	}
+
+	return len(stack) == 0
+}
+
+func main() {
+	fmt.Println(isValid("([]){}"))
+
+}
---
+++ go-demo/practice/palindrome/palindrome.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	s := strconv.Itoa(x)
+	runes := []rune(s)
+
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		if runes[i] != runes[j] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	var x int
+	fmt.Scan(&x)
+	fmt.Println(isPalindrome(x))
+}
